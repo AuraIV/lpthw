96bd42c4bbfc34e3be8301287acbeb0ac9a2be2c
Aura Velarde <aura.velarde@gmail.com>
2015-12-23 20:17:49 -0500

    Finished exercise 10

---
+++ ex10.py
@@ -0,0 +1,26 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#while True:
+#	for i in ["/","-", "|", "\\", "|"]:
+#		print "%s\r % i,
+
+#hi = ''' hello!! '''
+#print hi
+
+#Prints the \n in between, unlike %s
+#complex_format = "%r \n %r"
+#print complex_format % (tabby_cat, persian_cat)
