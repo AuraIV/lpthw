d5db633ad5bc932e5e206d6a0b180acd4d431d28
Aura Velarde <aura.velarde@gmail.com>
2015-12-23 19:52:33 -0500

    Finished exercise seven

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s" % 'snow' 
+print "And everywhere that Mary went."
+#Prints this statement 10 times
+print "." * 10
+
+#Assigns each variable to a string of one
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6, #the coma makes sure it is printed in the same line
+print end7 + end8 + end9 + end10 + end11 + end12 
