94a3c5a52e86735a1b7a0d3b3cf4a5ce5ecc6ca4
Aura Velarde <aura.velarde@gmail.com>
2015-12-26 22:57:16 -0500

    Answers to 28

--- ex9.py
+++ ex9.py
@@ -1,14 +1,14 @@
 # Here's some new strange stuff, remember type it exactly 
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\n\Mar\nApr\nMay\nJun\nJul\nAug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days: ", days 
 print "Here are the months: ", months 
 
 print """
 
-There's something oing on here.
+There's something going on here.
 With three double-quites.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6
